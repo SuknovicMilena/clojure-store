9f7b857a0ae094e32725b61304b1982f508833fa
Milena Suknovic <milena.suknovic@hotmail.com>
2018-05-05 14:33:11 +0200

    Added all remaining entities models. Moved db into separate folder.

--- src/store/domain.clj
+++ src/store/domain.clj
@@ -13,3 +13,45 @@
 })
 
 (s/defschema NewKorisnik (dissoc Korisnik :korisnikId))
+
+
+(s/defschema Proizvodjac {
+  :proizvodjacId   Integer
+  :naziv           String
+  :zemlja          String
+})
+
+(s/defschema NewProzvodjac (dissoc Proizvodjac :proizvodjacId))
+
+
+(s/defschema Proizvod {
+  :proizvodId      Integer
+  :nazivProizvoda  String
+  :cena            Double
+  :boja            String
+  :tip             String
+  :proizvodjacId   String
+})
+
+(s/defschema NewProizvod (dissoc Proizvod :proizvodId))
+
+
+(s/defschema Racun {
+  :brojRacuna      Integer
+  :datumKupovine   String
+  :ukupanIznos     Double
+  :korisnikId      Integer
+})
+
+(s/defschema Racun (dissoc Racun :brojRacuna))
+
+
+(s/defschema StavkaRacuna {
+  :redniBrojStavke Integer
+  :brojRacuna      Integer
+  :iznosStavke     Double
+  :proizvodId      Integer
+  :kolicina        Integer
+})
+
+(s/defschema Racun (dissoc Racun :brojRacuna))
